9997b360d3cb70b568a62d7ee8f0737bed37f573
m4rtyr <silentcat@protonmail.com>
2019-11-24 23:03:11 -0600

    Added user input to make adjusting size easier

--- binary_tree_demo.rb
+++ binary_tree_demo.rb
@@ -2,11 +2,15 @@
 # @Date:   2019-11-24T22:46:05-06:00
 # @Email:  silentcat@protonmail.com
 # @Last modified by:   silentcat
-# @Last modified time: 2019-11-24T22:59:35-06:00
+# @Last modified time: 2019-11-24T23:02:27-06:00
 
 require 'grid'
 require 'binary_tree'
 
-grid = Grid.new(4, 4)
+puts "Rows: "
+rows = gets
+puts "Columns: "
+columns = gets
+grid = Grid.new(Integer(rows), Integer(columns))
 BinaryTree.on(grid)
 puts grid
