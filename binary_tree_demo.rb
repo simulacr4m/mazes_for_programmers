d3b5b08f5dc140f2b6b5a9c8d57b0eae3e946c3c
m4rtyr <silentcat@protonmail.com>
2019-11-24 22:59:52 -0600

    Finished binary tree (from book)

---
+++ binary_tree_demo.rb
@@ -0,0 +1,12 @@
+# @Author: S. Sharma <silentcat>
+# @Date:   2019-11-24T22:46:05-06:00
+# @Email:  silentcat@protonmail.com
+# @Last modified by:   silentcat
+# @Last modified time: 2019-11-24T22:59:35-06:00
+
+require 'grid'
+require 'binary_tree'
+
+grid = Grid.new(4, 4)
+BinaryTree.on(grid)
+puts grid
