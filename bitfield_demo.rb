34bf56b24bde5cfc7217bdf89dcdaa9820148ac2
m4rtyr <silentcat@protonmail.com>
2019-11-25 14:52:10 -0600

    Added option for user input

--- bitfield_demo.rb
+++ bitfield_demo.rb
@@ -2,11 +2,15 @@
 # @Date:   2019-11-25T13:46:19-06:00
 # @Email:  silentcat@protonmail.com
 # @Last modified by:   silentcat
-# @Last modified time: 2019-11-25T14:40:15-06:00
+# @Last modified time: 2019-11-25T14:51:29-06:00
 
 require 'bitfield_grid'
 require 'bitfield_tree'
 
-g = BitfieldGrid.new(4, 4)
+puts "Enter rows: "
+rows = Integer(gets)
+puts "Enter columns: "
+cols = Integer(gets)
+g = BitfieldGrid.new(rows, cols)
 BitfieldBinaryTree.on(g)
 puts g
