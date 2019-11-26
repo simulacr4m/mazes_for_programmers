0f249ac6dffb63309465029ace94811ff9f0a028
m4rtyr <silentcat@protonmail.com>
2019-11-25 18:28:51 -0600

    Implemented ability to find shortest path

--- dijkstra.rb
+++ dijkstra.rb
@@ -2,7 +2,7 @@
 # @Date:   2019-11-25T18:14:55-06:00
 # @Email:  silentcat@protonmail.com
 # @Last modified by:   silentcat
-# @Last modified time: 2019-11-25T18:21:19-06:00
+# @Last modified time: 2019-11-25T18:27:50-06:00
 
 require 'distance_grid'
 require 'binary_tree'
@@ -30,3 +30,7 @@ distances = start.distances
 
 grid.distances = distances
 puts grid
+
+puts "path from northwest corner to southwest corner"
+grid.distances = distances.path_to(grid[grid.rows - 1, 0])
+puts grid.to_s
--- distances.rb
+++ distances.rb
@@ -2,7 +2,7 @@
 # @Date:   2019-11-25T18:04:55-06:00
 # @Email:  silentcat@protonmail.com
 # @Last modified by:   silentcat
-# @Last modified time: 2019-11-25T18:17:23-06:00
+# @Last modified time: 2019-11-25T18:26:19-06:00
 
 class Distances
 
@@ -22,5 +22,23 @@ class Distances
 
   def cells
     @cells.key
+  end
+
+  def path_to(goal)
+    current = goal
+
+    breadcrumbs = Distances.new(@root)
+    breadcrumbs[current] = @cells[current]
+
+    until current == @root
+      current.links.each do |neighbor|
+        if @cells[neighbor] < @cells[current]
+          breadcrumbs[neighbor] = @cells[neighbor]
+          current = neighbor
+          break
+        end
+      end
+    end
+      breadcrumbs
   end
 end
