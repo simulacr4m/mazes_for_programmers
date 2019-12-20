7be32d923aa8adf256a8537bdb55d9db5cd522fb
m4rtyr <silentcat@protonmail.com>
2019-12-19 22:05:55 -0600

    Made hunt-and-kill show colored grid

--- hunt_and_kill_demo.rb
+++ hunt_and_kill_demo.rb
@@ -2,13 +2,14 @@
 # @Date:   2019-12-18T21:25:39-06:00
 # @Email:  silentcat@protonmail.com
 # @Last modified by:   silentcat
-# @Last modified time: 2019-12-18T21:26:11-06:00
+# @Last modified time: 2019-12-18T21:33:51-06:00
 
-require 'grid'
+require 'colored_grid'
 require 'hunt_and_kill'
 
-grid = Grid.new(20, 20)
+grid = ColoredGrid.new(20, 20)
 HuntAndKill.on(grid)
+grid.distances = grid[0, 0].distances
 
 filename = "hunt_and_kill.png"
 grid.to_png.save(filename)
